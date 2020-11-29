b2170921cfc9fdb49045f99695171fa6dad8954f
Vanegasmk <vanegasmk@gmail.com>
2020-11-29 15:29:29 -0600

    Add queries Admin

--- BingoProyect/ClientApp/src/app/login/queries.ts
+++ BingoProyect/ClientApp/src/app/login/queries.ts
@@ -1,1 +1,11 @@
 import gql from "graphql-tag";
+
+export const ADMIN_QUERY = gql`
+  query($email: String, $password: String) {
+    admin(email: $email, password: $password) {
+      id
+      email
+      password
+    }
+  }
+`;
