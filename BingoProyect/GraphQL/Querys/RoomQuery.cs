13389d5ce02a49e842bdcca5d5a96764eb967e7e
jodavasi <jodavasi@hotmail.com>
2020-12-13 00:16:39 -0600

    num added

--- BingoProyect/GraphQL/Querys/RoomQuery.cs
+++ BingoProyect/GraphQL/Querys/RoomQuery.cs
@@ -4,37 +4,45 @@ using BingoProyect.GraphQL.Types;
 using BingoProyect.Models;
 
 
-namespace  BingoProyect.GraphQL.Querys {
+namespace BingoProyect.GraphQL.Querys
+{
     class RoomQuery : ObjectGraphType
     {
         public RoomQuery(RoomRepository roomRepository, AdminRepository adminRepository, NumeroRepository numeroRepository)
         {
             Field<ListGraphType<RoomType>>("rooms",
-                              arguments: new QueryArguments(new QueryArgument<StringGraphType> { Name = "name"}),
-                              resolve: context => {
+                              arguments: new QueryArguments(new QueryArgument<StringGraphType> { Name = "name" }),
+                              resolve: context =>
+                              {
                                   return roomRepository.All(context);
                               });
 
             Field<ListGraphType<AdminType>>("admin",
                               arguments: new QueryArguments(
                                   new QueryArgument<StringGraphType> { Name = "email" },
-                                  new QueryArgument<StringGraphType> { Name = "password"}),
-                              resolve: context => {
+                                  new QueryArgument<StringGraphType> { Name = "password" }),
+                              resolve: context =>
+                              {
                                   return adminRepository.All(context);
                               });
 
             Field<ListGraphType<NumeroType>>("numero",
                               arguments: new QueryArguments(
-                                  new QueryArgument<IntGraphType> { Name = "num"}),
-                              resolve: context => {
+                                  new QueryArgument<IntGraphType> { Name = "num" }),
+                              resolve: context =>
+                              {
                                   return numeroRepository.All(context);
+                              });
 
             Field<CardboardType>("cardboard",
-                              arguments: new QueryArguments(
-                                  new QueryArgument<IntGraphType> { Name = "id" }),
-                              resolve: context => {
-                                  return roomRepository.FindCardboard(context.GetArgument<long>("id"));
-                              });
+            arguments: new QueryArguments(
+                new QueryArgument<IntGraphType> { Name = "id" }),
+            resolve: context =>
+            {
+                return roomRepository.FindCardboard(context.GetArgument<long>("id"));
+            });
         }
     }
 }
+
+
